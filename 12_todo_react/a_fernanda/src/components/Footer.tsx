1e539cea63800f8a5640895d16e0b11fc0cf4d79
fernandaday <fernandaiday@gmail.com>
2022-07-29 11:16:35 -0300

    Projeto Todo com TS

---
+++ 12_todo_react/a_fernanda/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS Todo</span> &copy; 2022
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
